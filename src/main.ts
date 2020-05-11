da30f1c8e9511dab32bd6e6a2c3eb29380f287d9
Max <ndawn@ndawn.ru>
2020-05-12 02:46:30 +0300

    Removed unused dependencies

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
 import store from './store'
-import Unicon from 'vue-unicons'
-// @ts-ignore
-import { uniListUl } from 'vue-unicons/src/icons'
 
 
-Unicon.add([uniListUl])
-
-Vue.use(Unicon)
 Vue.config.productionTip = false
 
 new Vue({
